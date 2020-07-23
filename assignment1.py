e7bfc43dbfa11ae4763f9c5fbd02ed9a382fb2ee
atharvakulkarni21 <68541422+atharvakulkarni21@users.noreply.github.com>
2020-07-23 23:57:36 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,8 @@
+# Assignment 1 day 4
+
+str1 = "what we think we become we are python programmer"
+s1 = str1.count("we")
+print(s1)
+# index 
+s2 = str1.index("we")
+print("index ",s2)
---
+++ assignment2.py
@@ -0,0 +1,8 @@
+str = "my name is atharva"
+# islower()
+s1 = str.islower()
+print("lowercase ",s1)
+# isupper()
+str1 = "HEY ... LETSUPGRADE NICE TO BE WITH YOU.."
+s2 = str1.isupper()
+print("uppercase ",s2)
